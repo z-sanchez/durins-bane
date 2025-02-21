b20425747960409ca11eec32d5d612987e0af161
Ziek <zieksanchez3@gmail.com>
2025-02-21 00:19:23 -0800

    Product of Array Except Self

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+def productsOfArrayExceptSelf(nums):
+    result = []
+    product = 1
+
+# pre products
+    for x in range(len(nums)):
+        if x == 0:
+            result.append(product)
+        else:
+            product = product * nums[x - 1]
+            result.append(product)
+
+    postfix = 1
+
+    for x in range(len(nums) - 1, -1, -1):
+        result[x] *= postfix
+        postfix *= nums[x]
+
+    return result
 
 
 if __name__ == "__main__":
+    nums = [1, 2, 3, 4]
 
-    print("Running")
+    print(productsOfArrayExceptSelf(nums))
