9e4a4d6b4d3e193144278526b7e8bbfb6703dcd9
Ziek <zieksanchez3@gmail.com>
2025-02-21 01:24:16 -0800

    Crackhead problem

--- main.py
+++ main.py
@@ -3,34 +3,21 @@
 
 
 def productsOfArrayExceptSelf(nums):
-    # first collects prefix, then multiplies them to get final products
     result = []
 
-    # this will collect the product of every value before n (...n)
     prefix = 1
 
-    # iterate through array
     for x in range(len(nums)):
-        # if x is first of array, there is no previous value to multiply with prefix
         if x == 0:
             result.append(prefix)
         else:
-            # multiply prefix with previous value
-            prefix = prefix * nums[x - 1]
+            prefix *= nums[x - 1]
             result.append(prefix)
 
-    # prefix array now holds the product of all values prior to any given n
-
-    # we want to do the same thing but for values that come after n
     postfix = 1
 
-    # iterate backwards
     for x in range(len(nums) - 1, -1, -1):
-        # use x to find the prefix for this particular index
-        # multiply with postfix
         result[x] *= postfix
-
-        # calculate the postfix for next iteration, this will collect the product for all values after any given n
         postfix *= nums[x]
 
     return result
