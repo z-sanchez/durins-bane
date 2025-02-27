f63038dff7951ee7e45474bc900cdd61b04cb300
Ziek <zieksanchez3@gmail.com>
2025-02-27 14:52:40 -0800

    Attempt at two integer sum II:

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
-def isPalindrome(strInput):
+def twoSum(nums, target):
     i = 0
-    j = len(strInput) - 1
+    j = 1
 
-    while (i < j):
-        while (i < j and not strInput[i].isalpha() and not strInput[i].isdigit()):
-            i += 1
-        while (j > i and not strInput[j].isalpha() and not strInput[j].isdigit()):
-            j -= 1
+    while (i < len(nums)):
+        while j < len(nums):
+            if (nums[i] + nums[j] == target):
+                return [i + 1, j + 1]
+            j += 1
 
-        print(i, j)
-
-        if (strInput[i].lower() != strInput[j].lower()):
-            return False
-
-        i = i + 1
-        j = j - 1
-
-    return True
+        i += 1
+        j = i + 1
 
 
 if __name__ == "__main__":
-    s = "0P"
+    # these must be least to greatest
+    nums = [2, 3, 4]
+    target = 6
 
-    s = s.lower().replace(" ", "")
+    # result must be least to greatest
+    result = twoSum(nums, target)
 
-    print(s[0].isdigit())
-    print(isPalindrome(s))
+    print(result)
