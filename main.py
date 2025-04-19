5638867f305dbc7d63b79804272dff3905766652
Ziek <zieksanchez3@gmail.com>
2025-04-19 07:14:01 -0700

    Quiz style got right

--- main.py
+++ main.py
@@ -2,28 +2,23 @@
 # Space Complexity: O(1), creating nothing
 
 def twoSumTwo(array, target):
-    # pointer to beginning and pointer to the end
-    l, r = 0, len(array) - 1
+    l = 0
+    r = len(array) - 1
 
-    # keeps indices from overlapping
-    while l < r:
+    while array[l] + array[r] != target:
         currentSum = array[l] + array[r]
-
         if currentSum > target:
             r -= 1
-        elif currentSum < target:
+        if currentSum < target:
             l += 1
-        else:
-            # wants these results with a 1 based index for some reason
-            return [l+1, r+1]
 
-    return []
+    return [l + 1, r + 1,]
 
 
 if "__main__" == __name__:
 
     # must be in order for it to work
-    array = [1, 2, 3, 4]
+    array = [0, 2, 3, 4]
     target = 3
     result = twoSumTwo(array, target)
     print(result)
