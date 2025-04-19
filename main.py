a211e7ee2b80d152037c4113f2f53875a2c38263
Ziek <zieksanchez3@gmail.com>
2025-04-19 07:40:07 -0700

    Struggled a bit solving encode decode

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 # Time Complexity: O(n), nested loops
 # Space Complexity: O(1), creating nothing
 
-def twoSumTwo(array, target):
-    l = 0
-    r = len(array) - 1
+def encode(array):
+    encodedStr = ""
+    for x in array:
+        encodedStr += str(len(x)) + '#' + x
+    return encodedStr
 
-    while array[l] + array[r] != target:
-        currentSum = array[l] + array[r]
-        if currentSum > target:
-            r -= 1
-        if currentSum < target:
-            l += 1
 
-    return [l + 1, r + 1,]
+def decode(str):
+    result = []
+    startingPlace = 0
+    while startingPlace < len(str):
+        cursor = startingPlace
+        while str[cursor] != '#':
+            cursor += 1
+
+        strLen = int(str[startingPlace: cursor])
+        result.append(str[cursor + 1: cursor + 1 + strLen])
+        startingPlace = cursor + 1 + strLen
+
+    return result
 
 
 if "__main__" == __name__:
-
     # must be in order for it to work
-    array = [0, 2, 3, 4]
-    target = 3
-    result = twoSumTwo(array, target)
-    print(result)
+    array = ["Ziek", "Loves", "Cheese"]
+    print("Input", array)
+    encodedResult = encode(array)
+    print("Encoded Result", encodedResult)
+    decodedResult = decode(encodedResult)
+    print("Decoded Result", decodedResult)
+    print("Solved: ", array == decodedResult)
