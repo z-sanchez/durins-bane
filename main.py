5e9c2ef80bf8871f33e77f1bc6ddd5600d440f09
Ziek <zieksanchez3@gmail.com>
2025-04-28 10:40:03 -0700

    Solve product of array

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 def productOfArray(array):
-
-    before = {}
-    after = {}
+    left = {}
+    right = {}
     result = []
 
     for x in range(len(array)):
         if x == 0:
-            before[x] = 1
+            left[x] = 1
             continue
 
-        before[x] = array[x - 1] * before[x - 1]
+        left[x] = left[x - 1] * array[x - 1]
 
     for index, value in reversed(list(enumerate(array))):
         if index == len(array) - 1:
-            after[index] = 1
+            right[index] = 1
             continue
 
-        after[index] = array[index + 1] * after[index + 1]
+        right[index] = right[index + 1] * array[index + 1]
 
     for x in range(len(array)):
-        result.append(before[x] * after[x])
+        result.append(left[x] * right[x])
 
     return result
 
 
 if "__main__" == __name__:
-    input = [-1, 0, 1, 2, 3]
+    input = [1, 2, 4, 6]
     productOfArray(input)
