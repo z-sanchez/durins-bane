b03cc230528b6d792beff85973b415e366ea1957
Ziek <zieksanchez3@gmail.com>
2025-04-30 12:28:21 -0700

    Solved encode decode

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
-# Time Complexity: O(n), we traverse the array once max
-# Space Complexity: O(1), no new data structure needed
+def encode(input):
+    output = ''
 
-def maxArea(heights):
-    left = 0
-    right = len(heights) - 1
-    result = 0
+    for x in input:
+        output += str(len(x)) + '#' + x
 
-    while left < right:
-        # multiply width (right - left) by the min height (bottleneck)
-        computedArea = (right - left) * min(heights[left], heights[right])
-        result = max(result, computedArea)
+    return output
 
-        # we'll want to keep our tallest wall and adjust the small one (like two sum)
-        if heights[left] < heights[right]:
-            left += 1
-        else:
-            right -= 1
 
-    return result
+def decode(key):
+    output = []
+    currentPoint = 0
+
+    while currentPoint < len(key):
+        delimiterPointer = currentPoint
+
+        while key[delimiterPointer] != '#':
+            delimiterPointer += 1
+
+        strLength = int(key[currentPoint:delimiterPointer])
+        delimiterPointer += 1
+        string = key[delimiterPointer:delimiterPointer + strLength]
+        output.append(string)
+
+        currentPoint = delimiterPointer + strLength
+
+    return output
 
 
 if "__main__" == __name__:
-    input = [1, 7, 2, 5, 4, 7, 3, 6]
-    result = maxArea(input)
-    print(result)
+    input = ["ziek", "loves", "bella", "alot"]
+    encodedStr = encode(input)
+    decodedList = decode(encodedStr)
+    print(decodedList)
