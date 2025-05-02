4235242edb3e22d00b057169cdbda8d64734187b
Ziek <zieksanchez3@gmail.com>
2025-05-02 08:36:26 -0700

    Solve trapping rainwater

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
-def containerWithMostWater(heights):
+def trappingRainwater(height):
+
+    if not height:
+        return 0
+
+    l, r = 0, len(height) - 1
+    leftMax, rightMax = height[l], height[r]
 
     result = 0
 
-    i = 0
-    j = len(heights) - 1
+    # To Calculate Water Trapped:
+    # wall bounded by          current wall
+    # min(leftMax, rightMax) - height[i]
+    # you cannot trap water any higher than the smallest wall
 
-    while i < j:
-        currentArea = min(heights[i], heights[j]) * (j - i)
-        result = max(currentArea, result)
+    while l < r:
+        # solve with the smallest wall (bounded wall)
+        if leftMax < rightMax:
+            # advance wall to focus on a new spot
+            l += 1
 
-        if heights[i] < heights[j]:
-            i += 1
-        elif heights[j] < heights[i]:
-            j -= 1
+            # left max is basically the tallest wall prior to current one
+            # here we find the 'ceiling'
+            leftMax = max(leftMax, height[l])
+
+            # then we subtract the height of our current wall from the ceiling
+            result += leftMax - height[l]
         else:
-            i += 1
+            r -= 1
+            # right max is basically the tallest wall prior to current one
+            rightMax = max(rightMax, height[r])
+            result += rightMax - height[r]
 
     return result
 
 
 if "__main__" == __name__:
-
-    heights = [2, 2, 2]
-
-    result = containerWithMostWater(heights)
-
-    print("Correct: ", result == 36)
-    print("You answered: ", result)
+    height = [0, 2, 0, 3, 1, 0, 1, 3, 2, 1]
+    result = trappingRainwater(height)
+    print(result)
