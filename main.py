17a1fe60d544af495e5110188db6b28535d95e89
Ziek <zieksanchez3@gmail.com>
2025-05-05 09:44:06 -0700

    Solve ecode decode

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 # Time Complexity = O(n) we traverse the array once
 # Space Complexity = O(1) No space needed
 
-def trappedWater(heights):
-    i = 0
-    j = len(heights) - 1
-    leftMax = heights[i]
-    rightMax = heights[j]
-    waterCount = 0
+def encode(list):
+    result = ""
 
-    while i < j:
-        if leftMax < rightMax:
-            i += 1
-            leftMax = max(leftMax, heights[i])
-            waterCount += leftMax - heights[i]
-        else:
-            j -= 1
-            rightMax = max(rightMax, heights[j])
-            waterCount += rightMax - heights[j]
+    for x in list:
+        result += str(len(x)) + "#" + x
+    return result
 
-    return waterCount
+
+def decode(encodedString):
+
+    pointer = 0
+    result = []
+
+    while pointer < len(encodedString):
+
+        delimiterPointer = pointer
+
+        while encodedString[delimiterPointer] != "#":
+            delimiterPointer += 1
+
+        strLength = int(encodedString[pointer:delimiterPointer])
+
+        delimiterPointer += 1
+
+        str = encodedString[delimiterPointer: delimiterPointer + strLength]
+
+        result.append(str)
+
+        pointer = delimiterPointer + strLength
+
+    return result
 
 
 if "__main__" == __name__:
-    heights = [0, 2, 0, 3, 1, 0, 1, 3, 2, 1]
-    result = trappedWater(heights)
-    print(result)
+    list = ["ziek", "loves", "cheese", "alot"]
+    encodedString = encode(list)
+    decodedMessage = decode(encodedString)
+    print(decodedMessage)
