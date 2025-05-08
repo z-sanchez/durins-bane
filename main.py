ae1aafbbf50c1496f6afa0e7c6ba21756ea2b2ed
Ziek <zieksanchez3@gmail.com>
2025-05-08 07:08:11 -0700

    Solve valid parenthesis

--- main.py
+++ main.py
@@ -1,40 +1,31 @@
 # Time Complexity = O(1) we never traverse, constant time for everything
 # Space Complexity = O(n) create stacks
 
-class MinStack:
+def validParenthesis(input):
+    openToClose = {
+        ")": "(",
+        "}": "{",
+        "]": "["
+    }
 
-    def __init__(self):
-        # stack for actual values
-        self.stack = []
-        # stack for the corresponding min at that value
-        self.minStack = []
+    stack = []
 
-    def push(self, val: int) -> None:
-        self.stack.append(val)
-        # find minimum to append to the minStack, check if minStack is empty before indexing, else use val
-        val = min(val, self.minStack[-1] if self.minStack else val)
-        self.minStack.append(val)
+    for x in input:
+        if x in openToClose:
+            if stack and openToClose[x] == stack[-1]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(x)
 
-    def pop(self) -> None:
-        # keep both stacks in sync
-        self.stack.pop()
-        self.minStack.pop()
-
-    def top(self) -> int:
-        # top asks for last element
-        return self.stack[-1]
-
-    def getMin(self) -> int:
-        # use the corresponding min
-        return self.minStack[-1]
+    if stack:
+        return False
+    else:
+        return True
 
 
 if "__main__" == __name__:
-    minStack = MinStack()
-    minStack.push(1)
-    minStack.push(2)
-    minStack.push(0)
-    print(minStack.getMin())
-    print(minStack.pop())
-    print(minStack.top())
-    print(minStack.getMin())
+    input = "([{}])"
+    result = validParenthesis(input)
+    print(result)
