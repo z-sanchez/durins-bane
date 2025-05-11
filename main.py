4b491de482fe99b18fa2fc56efeb870e1ac5c89b
Ziek <zieksanchez3@gmail.com>
2025-05-11 13:35:23 -0700

    Solved trapped rainwater

--- main.py
+++ main.py
@@ -8,14 +8,12 @@ def trappedRainWater(height):
     leftMax = height[left]
     rightMax = height[right]
 
-    if not height:
-        return 0
-
     while left < right:
         if leftMax < rightMax:
             left += 1
             leftMax = max(leftMax, height[left])
             result += leftMax - height[left]
+
         else:
             right -= 1
             rightMax = max(rightMax, height[right])
