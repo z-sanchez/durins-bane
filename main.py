beb6c4c79efe1c0748adf69caf689bd33a3c8e5b
Ziek <zieksanchez3@gmail.com>
2025-05-14 10:31:50 -0700

    Solve generate parenthesis

--- main.py
+++ main.py
@@ -8,28 +8,23 @@ def generateParenthesis(n: int) -> List[str]:
     stack = []
     result = []
 
-    # backtrack to find the possibilities (effectively creates a tree of possibilities)
-    def backtrack(openAmount, closedAmount):
-        if openAmount == closedAmount == n:
-            # turning stack into a string
+    def backtracing(openCount, leftCount):
+        if openCount == leftCount == n:
             result.append("".join(stack))
             return
 
-        # if open amounts are less than n, we can append another open
-        if openAmount < n:
-            stack.append("(")
-            backtrack(openAmount + 1, closedAmount)
-            # need to clean up because we use it later
+        if openCount > leftCount:
+            stack.append(')')
+            backtracing(openCount, leftCount + 1)
             stack.pop()
 
-        # if closed amounts are less than open, we must close
-        if closedAmount < openAmount:
-            stack.append(")")
-            backtrack(openAmount, closedAmount + 1)
-            # need to clean up because we use it later
+        if openCount < n:
+            stack.append('(')
+            backtracing(openCount + 1, leftCount)
             stack.pop()
 
-    backtrack(0, 0)
+    backtracing(0, 0)
+
     return result
 
 
