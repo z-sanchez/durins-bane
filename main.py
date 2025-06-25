cda66924242f8c235f061ea3cb745e597b45e955
Ziek <zieksanchez3@gmail.com>
2025-06-25 16:36:03 -0700

    solve daily temperatures

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 
 
-def carFleet(target, positions, speeds):
-    cars = [[pos, speed] for pos, speed in zip(positions, speeds)]
-    stack = []
+def dailyTemperatures(temperatures):
 
-    for pos, speed in sorted(cars)[::-1]:
-        stack.append((target-pos) / speed)
-        if len(stack) >= 2 and stack[-1] <= stack[-2]:
-            stack.pop()
+    result = [0 for x in range(len(temperatures))]
+    tempTracker = []
 
-    return len(stack)
+    for index, temp in enumerate(temperatures):
+
+        while len(tempTracker) > 0 and temp > tempTracker[-1][1]:
+            tempTrackerIndex, tempTrackerTemp = tempTracker.pop()
+            result[tempTrackerIndex] = index - tempTrackerIndex
+
+        tempTracker.append([index, temp])
+
+    return result
 
 
 if "__main__" == __name__:
-    target = 10
-    positions = [4, 1, 0, 7]
-    speeds = [2, 2, 1, 1]
+    temperatures = [30, 38, 30, 36, 35, 40, 28]
 
-    result = carFleet(target, positions, speeds)
+    result = dailyTemperatures(temperatures)
     print(result)
