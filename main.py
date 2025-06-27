19a2271cd98a663fd334e3236cbfaf65d311ccb7
Ziek <zieksanchez3@gmail.com>
2025-06-27 11:28:18 -0700

    Solved valid sudoku

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
-def carFleet(target, positions, speeds):
-    cars = [[pos, speed] for pos, speed in zip(positions, speeds)]
-    stack = []
+def isValidSudoku(board):
+    boxes = {}
+    columns = {}
+    rows = {}
 
-    for pos, speed in sorted(cars)[::-1]:
-        stack.append((target - pos) / speed)
-        if len(stack) >= 2 and stack[-1] <= stack[-2]:
-            stack.pop()
+    for row in range(9):
+        for column in range(9):
+            currentSquare = board[row][column]
 
-    return len(stack)
+            if currentSquare == ".":
+                continue
+
+            if row not in rows:
+                rows[row] = set()
+
+            if column not in columns:
+                columns[column] = set()
+
+            print(column)
+            print(row//3, column//3)
+
+            if (row//3, column//3) not in boxes:
+                boxes[(row//3, column//3)] = set()
+
+            if currentSquare in rows[row] or currentSquare in columns[column] or currentSquare in boxes[(row//3, column//3)]:
+                return False
+
+            rows[row].add(currentSquare)
+            columns[column].add(currentSquare)
+            boxes[(row//3, column//3)].add(currentSquare)
+
+    return True
 
 
-if "__main__" == __name__:
-    target = 10
-    position = [1, 4]
-    speed = [3, 2]
+if __name__ == "__main__":
+    board = [["1", "2", ".", ".", "3", ".", ".", ".", "."],
+             ["4", ".", ".", "5", ".", ".", ".", ".", "."],
+             [".", "9", "8", ".", ".", ".", ".", ".", "3"],
+             ["5", ".", ".", ".", "6", ".", ".", ".", "4"],
+             [".", ".", ".", "8", ".", "3", ".", ".", "5"],
+             ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+             [".", ".", ".", ".", ".", ".", "2", ".", "."],
+             [".", ".", ".", "4", "1", "9", ".", ".", "8"],
+             [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
 
-    result = carFleet(target, position, speed)
-    print(result)
+
+print(isValidSudoku(board))
