289c5a8f7b1766d11bd259d730071457e0163c33
Ziek <zieksanchez3@gmail.com>
2025-08-18 09:24:35 -0700

    Solve car fleet

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-from typing import List
+# Time Complexity = O(nlogn) we traverse the array once (n) but we do sort the card (longn)
+# Space Complexity = O(n) space for the stack
 
+def carFleet(target, positions, speeds):
+    pairs = [[position, speed] for position, speed in zip(positions, speeds)]
+    stack = []
+    for position, speed in sorted(pairs)[::-1]:
+        distance = (target - position) / speed
+        stack.append(distance)
 
-def dailyTemperatures(temps: int) -> List[str]:
-    # default all to zero
-    result = [0] * len(temps)
-    tempStack = []  # [index, temp]
+        if len(stack) >= 2 and stack[-1] <= stack[-2]:
+            stack.pop()
 
-    for index, temp in enumerate(temps):
-        while tempStack and tempStack[-1][1] < temp:
-            poppedTempIndex, poppedTemp = tempStack.pop()
-            result[poppedTempIndex] = index - poppedTempIndex
-        tempStack.append([index, temp])
-
-    return result
+    return len(stack)
 
 
 if "__main__" == __name__:
-    temperatures = [30, 38, 30, 36, 35, 40, 28]
+    target = 10
+    position = [4, 1, 0, 7]
+    speed = [2, 2, 1, 1]
 
-    result = dailyTemperatures(temperatures)
+    result = carFleet(target, position, speed)
     print(result)
