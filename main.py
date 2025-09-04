94e50a43d307c1449e459f7c5dfe252f579eeef4
Ziek <zieksanchez3@gmail.com>
2025-09-04 15:51:19 -0700

    Solve threesum

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
-# Time Complexity = O(nlogn) we traverse the array once (n) but we do sort the card (longn)
-# Space Complexity = O(n) space for the stack
+# Time Complexity: O(n^2), nested loops
+# Space Complexity: O(n), creating a result array
 
-def carFleet(target, positions, speeds):
-    pairs = [[pos, speed] for pos, speed in zip(positions, speeds)]
-    stack = []
+# The idea here is that you focus on one value at a time. Take that value and find what other two summed with it = 0. Then move to next index.
+# We don't have to worry about comparing results using previous array values because those have been exhausted and check for
+# Rule of thumb: Once we've passed an index in the array, those values have been checked if there in play already, no need to compare back
 
-    for position, speed in sorted(pairs)[::-1]:
-        time = (target - position) / speed
-        stack.append(time)
+def threeSum(array):
+    result = []
 
-        if len(stack) >= 2 and stack[-1] <= stack[-2]:
-            stack.pop()
+    array.sort()
 
-    return len(stack)
+    for index, value in enumerate(array):
+        if index > 0 and array[index] == array[index-1]:
+            continue
+
+        left = index + 1
+        right = len(array) - 1
+
+        while left < right:
+            threeSum = value + array[left] + array[right]
+
+            if threeSum < 0:
+                left += 1
+            elif threeSum > 0:
+                right -= 1
+            else:
+                result.append([value, array[left], array[right]])
+                left += 1
+                while left < right and array[left] == array[left - 1]:
+                    left += 1
+    return result
 
 
 if "__main__" == __name__:
-    target = 10
-    position = [1, 4]
-    speed = [3, 2]
 
-    result = carFleet(target, position, speed)
-    print(result)
+    array = [-1, 0, 1, 2, -1, -4]
+
+    print(threeSum(array))
