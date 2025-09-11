55684ea24b21155f803080b1e514aa231d8122cd
Ziek <zieksanchez3@gmail.com>
2025-09-11 14:27:06 -0700

    Solved aidsflasjf

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
-# Time Complexity: O(n), we traverse the array once max
-# Space Complexity: O(1), no new data structure needed
+# Time Complexity: O(n^2), nested loops
+# Space Complexity: O(n), creating a result array
 
-def maxArea(heights):
-    result = 0
-    left = 0
-    right = len(heights) - 1
+# The idea here is that you focus on one value at a time. Take that value and find what other two summed with it = 0. Then move to next index.
+# We don't have to worry about comparing results using previous array values because those have been exhausted and check for
+# Rule of thumb: Once we've passed an index in the array, those values have been checked if there in play already, no need to compare back
 
-    while left < right:
-        calculatedArea = (right - left) * min(heights[left], heights[right])
-        result = max(result, calculatedArea)
+def threeSum(array):
+    result = []
 
-        if heights[left] > heights[right]:
-            right -= 1
-        else:
-            left += 1
+    array.sort()
 
+    for index, value in enumerate(array):
+        if index > 0 and array[index] == array[index - 1]:
+            continue
+
+        left = index + 1
+        right = len(array) - 1
+
+        while left < right:
+            threeSum = value + array[left] + array[right]
+
+            if threeSum < 0:
+                left += 1
+            elif threeSum > 0:
+                right -= 1
+            else:
+                result.append([value, array[left], array[right]])
+                left += 1
+                while left < right and array[left] == array[left - 1]:
+                    left += 1
     return result
 
 
 if "__main__" == __name__:
-    input = [1, 7, 2, 5, 4, 7, 3, 6]
-    result = maxArea(input)
-    print(result)
+
+    array = [-1, 0, 1, 2, -1, -4]
+
+    print(threeSum(array))
