8c3d3608e37bd953ec9f1158076d515b49b6ad2e
Ziek <zieksanchez3@gmail.com>
2025-09-23 09:41:34 -0700

    Solve max water in container

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
-# Time Complexity = O(n) we traverse the array once
-# Space Complexity = O(n) Need stack for potentially every index
+# Time Complexity: O(n), we traverse the array once max
+# Space Complexity: O(1), no new data structure needed
 
-def largestRectangleArea(heights) -> int:
+def maxArea(heights):
+    left = 0
+    right = len(heights) - 1
     result = 0
-    stack = []  # index, height
 
-    for index, height in enumerate(heights):
-        pointer = index
+    while left < right:
+        calculatedArea = (right - left) * min(heights[left], heights[right])
 
-        while stack and height < stack[-1][1]:
-            stackIndex, stackHeight = stack.pop()
-            calculatedArea = stackHeight * (index - stackIndex)
-            result = max(calculatedArea, result)
-            pointer = stackIndex
-
-        stack.append([pointer, height])
-
-    for index, height in stack:
-        calculatedArea = height * (len(heights) - index)
         result = max(result, calculatedArea)
+
+        if heights[left] < heights[right]:
+            left += 1
+        else:
+            right -= 1
 
     return result
 
 
 if "__main__" == __name__:
-    height = [7, 1, 7, 2, 2, 4]
-    result = largestRectangleArea(height)
+    input = [1, 7, 2, 5, 4, 7, 3, 6]
+    result = maxArea(input)
     print(result)
