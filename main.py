585691b83163e01e3a48970ecd6215c27387bbd8
Ziek <zieksanchez3@gmail.com>
2025-09-25 14:38:57 -0700

    Solve largest rectanlge area

--- main.py
+++ main.py
@@ -1,49 +1,31 @@
-def searchMatrix(matrix, target):
-    ROWS = len(matrix)  # get length of outer list
-    COLS = len(matrix[0])  # get length of inner list
+# Time Complexity = O(n) we traverse the array once
+# Space Complexity = O(n) Need stack for potentially every index
 
-    # start like usual binary search
-    left = 0
-    right = ROWS - 1
+def largestRectangleArea(heights) -> int:
 
-    while left <= right:
-        # midpoint is the middle row
-        middleRow = (left + right) // 2
+    maxArea = 0
 
-        # regular binary search logic with the twist
-        # we are checking the begin and end of the list
-        if target > matrix[middleRow][-1]:
-            left = middleRow + 1
-        elif target < matrix[middleRow][0]:
-            right = middleRow - 1
-        else:
-            break
+    stack = []
 
-    # it's possible our adjusted pointers are out of range, so check it
-    if not (left <= right):
-        return False
+    for index, height in enumerate(heights):
+        pointer = index
 
-    # restart but on the inner list level
-    left = 0
-    right = COLS - 1
+        while stack and height < stack[-1][1]:
+            stackIndex, stackHeight = stack.pop()
+            calculatedArea = (index - stackIndex) * stackHeight
+            maxArea = max(maxArea, calculatedArea)
+            pointer = stackIndex
 
-    # normal binary search
-    while left <= right:
-        midPoint = midPoint = (left + right) // 2
+        stack.append([pointer, height])
 
-        # use middleRow we found from earlier to look at the right row
-        if target > matrix[middleRow][midPoint]:
-            left = midPoint + 1
-        elif target < matrix[middleRow][midPoint]:
-            right = midPoint - 1
-        else:
-            return True
+    for index, height in stack:
+        calculatedArea = (len(heights) - index) * height
+        maxArea = max(maxArea, calculatedArea)
 
-    return False
+    return maxArea
 
 
 if "__main__" == __name__:
-    matrix = [[1, 2, 4, 8], [10, 11, 12, 13], [14, 20, 30, 40]]
-    target = 12
-    result = searchMatrix(matrix, target)
+    height = [7, 1, 7, 2, 2, 4]
+    result = largestRectangleArea(height)
     print(result)
