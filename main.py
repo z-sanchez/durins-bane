4be542c0e3abfac07e442e984e081315bd6a26a4
Ziek <zieksanchez3@gmail.com>
2025-09-29 09:10:40 -0700

    Failed to solve car fleet

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
-# Time Complexity = O(n) we traverse the array once
-# Space Complexity = O(1) No space needed
+# Time Complexity = O(nlogn) we traverse the array once (n) but we do sort the card (longn)
+# Space Complexity = O(n) space for the stack
 
-def trappingRainwater(height):
+def carFleet(target, positions, speeds):
+    # zip combines two array into pairs, use for loop to fill the array using tuples returned
+    pairs = [[pos, speed] for pos, speed in zip(positions, speeds)]
 
-    if not height:
-        return 0
+    stack = []
 
-    l, r = 0, len(height) - 1
-    leftMax, rightMax = height[l], height[r]
+    for position, speed in sorted(pairs)[::-1]:
+        time = (target - position) / speed
+        stack.append(time)
+        while len(stack) > 1 and stack[-1] <= stack[-2]:
+            stack.pop()
 
-    result = 0
-
-    while l < r:
-
-        if leftMax < rightMax:
-            l += 1
-            leftMax = max(height[l], leftMax)
-            result += leftMax - height[l]
-        else:
-            r -= 1
-            rightMax = max(rightMax, height[r])
-            result += rightMax - height[r]
-
-    return result
+    return len(stack)
 
 
 if "__main__" == __name__:
-    height = [0, 2, 0, 3, 1, 0, 1, 3, 2, 1]
-    result = trappingRainwater(height)
+    target = 10
+    position = [1, 4]
+    speed = [3, 2]
+
+    result = carFleet(target, position, speed)
     print(result)
