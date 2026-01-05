00120e977a632897bd744eb4af39d74affdca72b
Ziek <zieksanchez3@gmail.com>
2026-01-05 11:16:18 -0800

    Solve container with most water

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-# Time Complexity: O(n), nested loops
-# Space Complexity: O(1), creating nothing
+# Time Complexity: O(n), we traverse the array once max
+# Space Complexity: O(1), no new data structure needed
 
-def carFleet(target, positions, speeds):
-    # zip combines two array into pairs, use for loop to fill the array using tuples returned
-    pair = [[p, s] for p, s in zip(positions, speeds)]
+def maxArea(heights):
 
-    # this will keep track of fleet speeds
-    stack = []
+    left = 0
+    right = len(heights) - 1
+    result = 0
 
-    for position, speed in sorted(pair)[::-1]:
-        time = (target - position) / speed
-        stack.append(time)
-        if len(stack) > 1 and stack[-2] >= stack[-1]:
-            stack.pop()
+    while left < right:
+        area = (right - left) * min(heights[left], heights[right])
 
-    return len(stack)
+        result = max(result, area)
+
+        if heights[left] > heights[right]:
+            right -= 1
+        else:
+            left += 1
+
+    return result
 
 
 if "__main__" == __name__:
-    target = 10
-    position = [1, 4]
-    speed = [3, 2]
-
-    result = carFleet(target, position, speed)
+    input = [1, 7, 2, 5, 4, 7, 3, 6]
+    result = maxArea(input)
     print(result)
