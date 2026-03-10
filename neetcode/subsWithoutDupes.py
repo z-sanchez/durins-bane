e3784e7e1404009ecc47b71c53d1adedb3be693e
Ziek <zieksanchez3@gmail.com>
2026-03-10 15:38:23 -0700

    Solve subs without dupes

---
+++ neetcode/subsWithoutDupes.py
@@ -0,0 +1,20 @@
+def subsWithoutDupes(s: str) -> int:
+    maxCount = 0
+    left = 0
+    charSet = set()
+
+    for right in range(len(s)):
+        while s[right] in charSet:
+            charSet.remove(s[left])
+            left += 1
+        charSet.add(s[right])
+        maxCount = max(maxCount, right - left + 1)
+
+    return maxCount
+
+
+if "__main__" == __name__:
+    s = "xyzxzyyzx"
+
+    result = subsWithoutDupes(s)
+    print(result)
