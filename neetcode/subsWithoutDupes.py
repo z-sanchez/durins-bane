c0e69dd50c334054103ce7f5301ef69351c0bbdd
Ziek <zieksanchez3@gmail.com>
2026-03-12 11:42:15 -0700

    Solve subs without dupes

--- neetcode/subsWithoutDupes.py
+++ neetcode/subsWithoutDupes.py
@@ -1,14 +1,16 @@
 def subsWithoutDupes(s: str) -> int:
+
+    charSet = set()
     maxCount = 0
     left = 0
-    charSet = set()
 
     for right in range(len(s)):
         while s[right] in charSet:
             charSet.remove(s[left])
             left += 1
-        charSet.add(s[right])
+
         maxCount = max(maxCount, right - left + 1)
+        charSet.add(s[right])
 
     return maxCount
 
