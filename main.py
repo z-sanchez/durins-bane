ac39b5a36bd70fd1f0f9c8bdc2efacb80f3ffeac
Ziek <zieksanchez3@gmail.com>
2026-03-24 13:07:23 -0700

    Solve remove nth form end

--- main.py
+++ main.py
@@ -1,42 +1,28 @@
+
+# Time Complexity: O(n), iterates through linked list no more than once
+# Space Complexity: O(1), not creating anything except dummy, 1 every time
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# [2 -> 4 -> 6 -> 8]
-
-# Time Complexity: O(n), iterate through list a few times but still linear
-# Space Complexity: O(1), no new space created
 
 class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # create dummy node that points to list, we will return dummy.next (start of list)
+        dummy = ListNode(0, head)
 
-        slow = head
-        fast = head.next
+        left = dummy
+        right = head
 
-        while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
+        while n > 0:
+            right = right.next
+            n -= 1
 
-        prev = None
-        second = slow.next
-        slow.next = None
+        while right:
+            right = right.next
+            left = left.next
 
-        while second:
-            next = second.next
-            second.next = prev
-            prev = second
-            second = next
-
-        first = head
-        second = prev
-
-        while second:
-            temp1 = first.next
-            temp2 = second.next
-
-            first.next = second
-            second.next = temp1
-
-            first = temp1
-            second = temp2
+        left.next = left.next.next
+        return dummy.next
