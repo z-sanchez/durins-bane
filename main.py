f084ff3cbdc3e4375cac4e26f21db9ea33d68345
Ziek <zieksanchez3@gmail.com>
2026-04-01 09:29:57 -0700

    Solve char replacement

--- main.py
+++ main.py
@@ -1,41 +1,24 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-# [2 -> 4 -> 6 -> 8]
+def characterReplacement(s: str, k: int) -> int:
+    # map used to track frequency of specific chars
+    count = {}
+    result = 0
+    # start of the window
+    left = 0
 
-# Time Complexity: O(n), iterate through list a few times but still linear
-# Space Complexity: O(1), no new space created
+    for right in range(len(s)):
+        count[s[right]] = 1 + count.get(s[right], 0)
 
-class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        slow = head
-        fast = head.next
+        if (right - left + 1) - max(count.values()) > k:
+            count[s[left]] -= 1
+            left += 1
 
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+        result = max(result, right - left + 1)
 
-        prev = None
-        second = slow.next
-        slow.next = None
+    return result
 
-        while second:
-            temp = second.next
-            second.next = prev
-            prev = second
-            second = temp
 
-        first = head
-        second = prev
-
-        while second:
-            temp1 = first.next
-            temp2 = second.next
-
-            first.next = second
-            second.next = temp1
-
-            first = temp1
-            second = temp2
+if "__main__" == __name__:
+    s = "AABA"
+    k = 2
+    max = characterReplacement(s, k)
+    print("result", max)
