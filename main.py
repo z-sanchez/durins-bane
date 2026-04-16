ba85b40f495b765e3749569e6aa294154a21809b
Ziek <zieksanchez3@gmail.com>
2026-04-16 13:30:30 -0700

    Solve LRU

--- main.py
+++ main.py
@@ -1,10 +1,53 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.value = val
+        self.next = None
+        self.prev = None
+
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        print(capacity)
+        self.size = capacity
+        self.cache = {}
+
+        self.left = Node(0, 0)
+        self.right = Node(0, 0)
+
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def remove(self, node):
+        prev = node.prev
+        next = node.next
+
+        prev.next = next
+        next.prev = prev
+
+    def insert(self, node):
+        prev = self.right.prev
+        next = self.right
+        prev.next = node
+        next.prev = node
+        node.next = next
+        node.prev = prev
 
     def get(self, key: int) -> int:
-        print(key)
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].value
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        print(value)
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.size:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
