c9f18ab829fcd6153197818fa7a2354876f0176d
Ziek <zieksanchez3@gmail.com>
2026-04-21 13:21:55 -0700

    Solve nth from end

--- main.py
+++ main.py
@@ -1,57 +1,29 @@
-# Time Complexity: O(1) for get and put operations
-# Space Complexity: O(n), where n is the capacity of the cache
-class Node:
-    def __init__(self, key, val):
-        self.key = key  # key is the key to the node in the cache
-        self.value = val  # value is our actual value
-        self.next = None
-        self.prev = None
 
+# Time Complexity: O(n), iterates through linked list no more than once
+# Space Complexity: O(1), not creating anything except dummy, 1 every time
 
-class LRUCache:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
-    def __init__(self, capacity: int):
-        print(capacity)
-        self.cache = {}
-        self.capacity = capacity
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # create dummy node that points to list, we will return dummy.next (start of list)
+        dummy = ListNode(0, head)
 
-        self.right = Node(0, 0)
-        self.left = Node(0, 0)
+        left = dummy
+        right = head
 
-        self.right.prev = self.left
-        self.left.next = self.right
+        while n > 0 and right:
+            n -= 1
+            right = right.next
 
-    def remove(self, node):
-        prev = node.prev
-        next = node.next
-        prev.next = next
-        next.prev = prev
+        while right:
+            left = left.next
+            right = right.next
 
-    def insert(self, node):
-        prev = self.right.prev
-        next = self.right
-        prev.next = node
-        next.prev = node
-        node.prev = prev
-        node.next = next
+        left.next = left.next.next
 
-    def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].value
-
-        return -1
-
-    def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
-
-        node = Node(key, value)
-        self.cache[key] = node
-        self.insert(self.cache[key])
-
-        if len(self.cache) > self.capacity:
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
+        return dummy.next
