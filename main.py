93ff6a99097753cfa5933508ccccdac9ea512161
Ziek <zieksanchez3@gmail.com>
2026-04-21 10:29:35 -0700

    Solve lru

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # Time Complexity: O(1) for get and put operations
 # Space Complexity: O(n), where n is the capacity of the cache
-
-
 class Node:
     def __init__(self, key, val):
         self.key = key  # key is the key to the node in the cache
@@ -17,11 +15,11 @@ class LRUCache:
         self.cache = {}
         self.capacity = capacity
 
-        left = Node(0, 0)
-        right = Node(0, 0)
+        self.right = Node(0, 0)
+        self.left = Node(0, 0)
 
-        left.next = right
-        right.prev = left
+        self.right.prev = self.left
+        self.left.next = self.right
 
     def remove(self, node):
         prev = node.prev
@@ -32,12 +30,10 @@ class LRUCache:
     def insert(self, node):
         prev = self.right.prev
         next = self.right
-
-        node.prev = prev
-        node.next = next
-
         prev.next = node
         next.prev = node
+        node.prev = prev
+        node.next = next
 
     def get(self, key: int) -> int:
         if key in self.cache:
@@ -51,7 +47,8 @@ class LRUCache:
         if key in self.cache:
             self.remove(self.cache[key])
 
-        self.cache[key] = Node(key, value)
+        node = Node(key, value)
+        self.cache[key] = node
         self.insert(self.cache[key])
 
         if len(self.cache) > self.capacity:
