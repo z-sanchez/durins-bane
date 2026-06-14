4943d47469cc794abfb184097a3dd3c200d43e52
Ziek <zieksanchez3@gmail.com>
2026-06-13 19:41:46 -0700

    products of array except self

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-# Time Complexity: O(n)
-# Space Complexity: O(n)
+# Time Complexity: O(n), iterates through list a couple of times but no more than n
+# Space Complexity: O(n), creating a result array
 
-def topKFrequent(nums, k):
-    # for counting the occurrence of each num
-    count = {}
 
-    frequencies = [[] for x in range(len(nums) + 1)]
-
-    for num in nums:
-        count[num] = 1 + count.get(num, 0)
-
-    for num, counted in count.items():
-        frequencies[counted].append(num)
-
+def productsOfArrayExceptSelf(nums):
+    # first collects prefix, then multiplies them to get final products
     result = []
 
-    for x in range(len(frequencies))[::-1]:
-        for i in frequencies[x]:
-            if len(result) >= k:
-                return result
-            else:
-                result.append(i)
+    prefix = 1
+
+    for x in range(len(nums)):
+        if x == 0:
+            result.append(prefix)
+        else:
+            prefix = prefix * nums[x-1]
+            result.append(prefix)
+
+    postfix = 1
+
+    for x in range(len(nums))[::-1]:
+        result[x] *= postfix
+
+        postfix *= nums[x]
 
     return result
 
 
 if __name__ == "__main__":
-    nums = [1, 1, 1, 2, 2, 100]
-    k = 2
-    print(topKFrequent(nums, 2))
+    nums = [1, 2, 3, 4]
+
+    print(productsOfArrayExceptSelf(nums))
