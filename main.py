339e2df3b458ef14b1267441689d632d0412b69e
Ziek <zieksanchez3@gmail.com>
2026-06-16 18:46:10 -0700

    Solve longest sequence

--- main.py
+++ main.py
@@ -1,49 +1,24 @@
-# Time Complexity: O(n^2), we nest loops to traverse row and columns
-# Space Complexity: O(n^2), each of our created mappings are two dimensional
+# Time Complexity: O(n), we traverse the array once max
+# Space Complexity: O(n), we make a set here
 
-def isValidSudoku(board):
-    # create hashmaps for each of the sections will need to check
-    # boxes will store coordinates as the key (1-3 for row, 1-3 for column, total of nine boxes)
-    boxes = {}
-    columns = {}
-    rows = {}
+def longestSequence(nums):
+    result = 0
 
-    for row in range(9):
-        for column in range(9):
+    numSet = set(nums)
 
-            currentValue = board[row][column]
+    for num in nums:
+        if num - 1 not in numSet:
+            length = 0
 
-            if currentValue == '.':
-                continue
+            while num + length in numSet:
+                length += 1
 
-            if row not in rows:
-                rows[row] = set()
+            result = max(result, length)
 
-            if column not in columns:
-                columns[column] = set()
-
-            if (row//3, column//3) not in boxes:
-                boxes[(row//3, column//3)] = set()
-
-            if currentValue in rows[row] or currentValue in columns[column] or currentValue in boxes[(row//3, column//3)]:
-                return False
-            else:
-                columns[column].add(currentValue)
-                rows[row].add(currentValue)
-                boxes[(row//3, column//3)].add(currentValue)
-
-    return True
+    return result
 
 
-if __name__ == "__main__":
-    board = [["1", "2", ".", ".", "3", ".", ".", ".", "."],
-             ["4", ".", ".", "5", ".", ".", ".", ".", "."],
-             [".", "9", "8", ".", ".", ".", ".", ".", "3"],
-             ["5", ".", ".", ".", "6", ".", ".", ".", "4"],
-             [".", ".", ".", "8", ".", "3", ".", ".", "5"],
-             ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-             [".", ".", ".", ".", ".", ".", "2", ".", "."],
-             [".", ".", ".", "4", "1", "9", ".", ".", "8"],
-             [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
-
-print(isValidSudoku(board))
+if "__main__" == __name__:
+    input = [0, 3, 2, 5, 4, 6, 1, 1]
+    result = longestSequence(input)
+    print(result)
