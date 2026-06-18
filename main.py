f9d19cadad5028596d6a49f7c3d38b9de76f491b
Ziek <zieksanchez3@gmail.com>
2026-06-17 20:14:17 -0700

    Solve two sum

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-# Time Complexity: O(n), we traverse the array once max
-# Space Complexity: O(n), we make a set here
+# Time Complexity: O(n), nested loops
+# Space Complexity: O(1), creating nothing
 
-def longestSequence(nums):
-    numSet = set(nums)
-    result = 0
+def twoSumTwo(array, target):
 
-    for num in numSet:
-        if num - 1 not in numSet:
-            length = 0
-            while num + length in numSet:
-                length += 1
-            result = max(result, length)
+    left = 0
+    right = len(array) - 1
 
-    return result
+    while left < right:
+        sum = array[left] + array[right]
+        if sum > target:
+            right -= 1
+        elif sum < target:
+            left += 1
+        else:
+            return [left + 1, right + 1]
+
+    return []
 
 
 if "__main__" == __name__:
-    input = [0, 3, 2, 5, 4, 6, 1, 1]
-    result = longestSequence(input)
+
+    # must be in order for it to work
+    array = [1, 2, 3, 4]
+    target = 3
+    result = twoSumTwo(array, target)
     print(result)
