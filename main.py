adea3531cdc42499a31f2e9d426a0233c1888e16
Ziek <zieksanchez3@gmail.com>
2026-07-02 06:43:45 -0700

    longest seqeucne

--- main.py
+++ main.py
@@ -2,23 +2,24 @@
 # Space Complexity: O(m), uses a count map
 # m = length of string, n = length of unique characters in string
 
-def characterReplacement(s: str, k: int) -> int:
-    charSet = set()
-    result = 0
-    left = 0
 
-    for right in range(len(s)):
-        while s[right] in charSet:
-            charSet.remove(s[left])
-            left += 1
-        result = max(result, right - left + 1)
-        charSet.add(s[right])
+def longestSequence(nums):
+
+    result = 0
+
+    numSet = set(nums)
+
+    for num in nums:
+        if num - 1 not in numSet:
+            length = 0
+            while num + length in numSet:
+                length += 1
+            result = max(result, length)
 
     return result
 
 
 if "__main__" == __name__:
-    s = "AABA"
-    k = 2
-    max = characterReplacement(s, k)
-    print("result", max)
+    input = [0, 3, 2, 5, 4, 6, 1, 1]
+    result = longestSequence(input)
+    print(result)
