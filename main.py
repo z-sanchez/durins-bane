24d244e5e3b574154dc9d42e1f73c957a7b29f35
Ziek <zieksanchez3@gmail.com>
2026-07-08 13:40:18 -0700

    solve subs without dupes

--- main.py
+++ main.py
@@ -1,30 +1,20 @@
-# Time Complexity: O(n)
-# Space Complexity: O(n)
+def subsWithoutDupes(s: str) -> int:
+    result = 0
+    charSet = set()
+    left = 0
 
-def topKFrequent(nums, k):
-
-    counts = {}
-    frequencies = [[] for x in range(len(nums) + 1)]
-
-    for num in nums:
-        counts[num] = 1 + counts.get(num, 0)
-
-    for num, count in counts.items():
-        frequencies[count].append(num)
-
-    result = []
-
-    for x in range(len(frequencies))[::-1]:
-        for j in frequencies[x]:
-            if len(result) < k:
-                result.append(j)
-            else:
-                return result
+    for right in range(len(s)):
+        while s[right] in charSet:
+            charSet.remove(s[left])
+            left += 1
+        charSet.add(s[right])
+        result = max(result, right - left + 1)
 
     return result
 
 
-if __name__ == "__main__":
-    nums = [1, 1, 1, 2, 2, 100]
-    k = 2
-    print(topKFrequent(nums, k))
+if "__main__" == __name__:
+    s = "xyzxzyyzx"
+
+    result = subsWithoutDupes(s)
+    print(result)
