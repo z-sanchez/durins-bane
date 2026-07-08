5f6034c2aac04f46bdfc548b452341c5cc9d54a2
Ziek <zieksanchez3@gmail.com>
2026-07-08 13:45:34 -0700

    solve top k frequent

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-def subsWithoutDupes(s: str) -> int:
-    result = 0
-    charSet = set()
-    left = 0
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 
-    for right in range(len(s)):
-        while s[right] in charSet:
-            charSet.remove(s[left])
-            left += 1
-        charSet.add(s[right])
-        result = max(result, right - left + 1)
+def topKFrequent(nums, k):
+    frequencies = [[] for x in range(len(nums) + 1)]
+
+    counts = {}
+
+    for num in nums:
+        counts[num] = 1 + counts.get(num, 0)
+
+    for num, count in counts.items():
+        frequencies[count].append(num)
+
+    result = []
+
+    print(frequencies)
+
+    for x in range(len(frequencies))[::-1]:
+        for j in frequencies[x]:
+            if len(result) < k:
+                result.append(j)
+            else:
+                return result
 
     return result
 
 
-if "__main__" == __name__:
-    s = "xyzxzyyzx"
-
-    result = subsWithoutDupes(s)
-    print(result)
+if __name__ == "__main__":
+    nums = [1, 1, 1, 2, 2, 100]
+    k = 2
+    print(topKFrequent(nums, 1))
