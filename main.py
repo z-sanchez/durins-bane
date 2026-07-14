06b4620b16acd8ecc72791c558c0d254a42999aa
Ziek <zieksanchez3@gmail.com>
2026-07-14 15:38:21 -0700

    solve char replacement

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 def characterReplacement(str, limit):
+
+    charSet = {}
+    left = 0
     result = 0
 
-    left = 0
-    charSet = set()
-
     for right in range(len(str)):
-        while str[right] in charSet:
-            charSet.remove(str[left])
+        charSet[str[right]] = 1 + charSet.get(str[right], 0)
+
+        if (right - left + 1) - max(charSet.values()) > limit:
+            charSet[str[left]] -= 1
             left += 1
 
-        charSet.add(str[right])
         result = max(result, right - left + 1)
 
     return result
