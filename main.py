2bf71a7073dc6298f9f5e7da4f240771a1da38c9
Ziek <zieksanchez3@gmail.com>
2026-07-17 14:49:07 -0700

    char replacements

--- main.py
+++ main.py
@@ -1,37 +1,24 @@
-# Time Complexity: O(n), we traverse the array once max
-# Space Complexity: O(1), no new data structure needed
+def characterReplacement(string, limit):
 
-def threeSum(array):
-    result = []
+    result = 0
+    count = {}
 
-    array.sort()
+    left = 0
 
-    for index, value in enumerate(array):
-        if index > 0 and array[index - 1] == value:
-            continue
+    for right in range(len(string)):
+        count[string[right]] = 1 + count.get(string[right], 0)
 
-        left = index + 1
-        right = len(array) - 1
+        if (right - left + 1) - max(count.values()) > limit:
+            count[string[left]] -= 1
+            left += 1
 
-        while left < right:
-            sum = value + array[left] + array[right]
-
-            if sum > 0:
-                right -= 1
-            elif sum < 0:
-                left += 1
-            else:
-                result.append([value, array[left], array[right]])
-                left += 1
-
-                while left < right and array[left] == array[left - 1]:
-                    left += 1
+        result = max(result, (right - left + 1))
 
     return result
 
 
 if "__main__" == __name__:
-
-    array = [-1, 0, 1, 2, -1, -4]
-
-    print(threeSum(array))
+    s = "AABA"
+    k = 2
+    max = characterReplacement(s, k)
+    print("result", max)
