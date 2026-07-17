7f6393c726fca170dc01ea89cf87ab9984e3ab23
Ziek <zieksanchez3@gmail.com>
2026-07-17 14:39:14 -0700

    solve threesum

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 # Time Complexity: O(n), we traverse the array once max
 # Space Complexity: O(1), no new data structure needed
 
-def maxArea(heights):
-    result = 0
-    left = 0
-    right = len(heights) - 1
+def threeSum(array):
+    result = []
 
-    while left < right:
-        calculatedArea = (right - left) * min(heights[left], heights[right])
-        result = max(calculatedArea, result)
+    array.sort()
 
-        if heights[left] < heights[right]:
-            left += 1
-        else:
-            right -= 1
+    for index, value in enumerate(array):
+        if index > 0 and array[index - 1] == value:
+            continue
+
+        left = index + 1
+        right = len(array) - 1
+
+        while left < right:
+            sum = value + array[left] + array[right]
+
+            if sum > 0:
+                right -= 1
+            elif sum < 0:
+                left += 1
+            else:
+                result.append([value, array[left], array[right]])
+                left += 1
+
+                while left < right and array[left] == array[left - 1]:
+                    left += 1
 
     return result
 
 
 if "__main__" == __name__:
-    input = [1, 7, 2, 5, 4, 7, 3, 6]
-    result = maxArea(input)
-    print(result)
+
+    array = [-1, 0, 1, 2, -1, -4]
+
+    print(threeSum(array))
