4f64e8c2f33f6ad7e22ea9797007330696bdf125
Ziek <zieksanchez3@gmail.com>
2026-07-16 18:48:49 -0700

    threesum

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 def threeSum(array):
     result = []
 
-    # prevent duplicates by sorting
     array.sort()
 
     for index, value in enumerate(array):
-        if index > 0 and array[index] == array[index - 1]:
+        if index > 0 and array[index - 1] == array[index]:
             continue
 
         left = index + 1
         right = len(array) - 1
 
         while left < right:
-            sum = value + array[left] + array[right]
+            sum = array[index] + array[left] + array[right]
 
             if sum > 0:
                 right -= 1
             elif sum < 0:
                 left += 1
             else:
-                result.append([value, array[left], array[right]])
+                result.append([array[index], array[left], array[right]])
                 left += 1
 
-                while array[left] == array[left - 1] and left < right:
+                while left < right and array[left] == array[left - 1]:
                     left += 1
 
     return result
