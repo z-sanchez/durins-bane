219ff000846305fe4feb61b0243c197d34479636
Ziek <zieksanchez3@gmail.com>
2026-07-24 14:26:08 -0700

    Solve max area

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
+# Time Complexity: O(n), we traverse the array once max
+# Space Complexity: O(1), no new data structure needed
 
-
-def subsWithoutDupes(str):
+def maxArea(heights):
+    left = 0
+    right = len(heights) - 1
 
     result = 0
 
-    charSet = set()
+    while left < right:
+        maxArea = (right - left) * min(heights[left], heights[right])
 
-    left = 0
+        result = max(maxArea, result)
 
-    for right in range(len(str)):
-
-        while str[right] in charSet:
-            charSet.remove(str[left])
+        if heights[left] < heights[right]:
             left += 1
-
-        charSet.add(str[right])
-
-        result = max(result, right - left + 1)
+        else:
+            right -= 1
 
     return result
 
 
 if "__main__" == __name__:
-    s = "xyzxzyyzx"
-
-    result = subsWithoutDupes(s)
+    input = [1, 7, 2, 5, 4, 7, 3, 6]
+    result = maxArea(input)
     print(result)
