4466624109732452dd9960d371055b6953aaef6e
Ziek <zieksanchez3@gmail.com>
2026-07-27 14:28:06 -0700

    solve products of array

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
 # Time Complexity: O(n), we traverse the array once max
 # Space Complexity: O(1), no new data structure needed
 
-def checkInclusion(s1, s2):
+def productsOfArrayExceptSelf(nums):
 
-    s1Count = [0] * 26
+    prefix = 1
 
-    for char in s1:
-        s1Count[ord(char) - ord('a')] += 1
+    result = []
 
-    s1Key = tuple(s1Count)
+    for index in range(len(nums)):
+        if index == 0:
+            result.append(prefix)
+        else:
+            prefix *= nums[index - 1]
+            result.append(prefix)
 
-    s2Count = [0] * 26
-    left = 0
+    print(result)
 
-    for right in range(len(s2)):
-        s2Count[ord(s2[right]) - ord('a')] += 1
+    postfix = 1
 
-        if (right - left + 1) > len(s1):
-            s2Count[ord(s2[left]) - ord('a')] -= 1
-            left += 1
+    for newIndex in range(len(nums))[::-1]:
+        result[newIndex] *= postfix
 
-        s2Key = tuple(s2Count)
+        postfix *= nums[newIndex]
 
-        if s1Key == s2Key:
-            return True
-
-    return False
+    return result
 
 
-if "__main__" == __name__:
-    s1 = "abc"
-    s2 = "lecabee"
+if __name__ == "__main__":
+    nums = [1, 2, 3, 4]
 
-    print(checkInclusion(s1, s2))
+    print(productsOfArrayExceptSelf(nums))
